cf93d674ef504431f63fb0be0eb0032f40a483e7
a <857813214@qq.com>
2023-03-05 12:06:20 +0800

    提交 94. 二叉树的中序遍历

---
+++ app/src/main/java/com/ming/algorithm/simple/InorderTraversal.kt
@@ -0,0 +1,47 @@
+package com.ming.algorithm.simple
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.ming.algorithm.bean.TreeNode
+import java.util.*
+
+/**
+ * @Description   二叉树的中序遍历
+ * @Author ming
+ * @Date 2022/2/10 23:20
+ */
+class InorderTraversal {
+
+    companion object {
+        /**
+        给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+         */
+        @JvmStatic
+        fun main(args: Array<String>) {
+            /**
+            输入：root = [1,null,2,3]
+            输出：[1,3,2]
+             */
+            print("结果是${solveOne(TreeNode(1))}\n")
+        }
+
+        /**
+         * 递归
+         */
+        fun solveOne(root: TreeNode?): List<Int> {
+            // 左中右
+            fun traversal(treeNode: TreeNode?, list: MutableList<Int>) {
+                if (treeNode == null) {
+                    return
+                }
+                traversal(treeNode.left, list)
+                list.add(treeNode.`val`)
+                traversal(treeNode.right, list)
+            }
+
+            val result = mutableListOf<Int>()
+            traversal(root, result)
+            return result
+        }
+    }
+}
