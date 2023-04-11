22ee8c4756939cfd804a6cefd64675ebf9665b38
a <857813214@qq.com>
2023-04-11 23:10:47 +0800

    提交 17. 电话号码的字母组合

--- app/src/main/java/com/ming/algorithm/middle/LetterCombinations.kt
+++ app/src/main/java/com/ming/algorithm/middle/LetterCombinations.kt
@@ -14,7 +14,7 @@ class LetterCombinations {
             输入：digits = "23"
             输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
              */
-            print("结果是${solveOne("23")}\n")
+            print("结果是${solveTwo("")}\n")
         }
 
         fun solveOne(digits: String): List<String> {
@@ -32,23 +32,58 @@ class LetterCombinations {
                 "9" to listOf("w", "x", "y", "z")
             )
 
-            fun letterCombinations(digits: String, lastList: MutableList<String>):List<String> {
+            fun letterCombinations(digits: String, lastList: MutableList<String>): List<String> {
 
                 if (digits.isNullOrEmpty() || digits.substring(0, 1)
                         .isNullOrEmpty() || map[digits.substring(0, 1)].isNullOrEmpty()
                 )
                     return lastList.toList()
                 val result = mutableListOf<String>()
-                val key =digits.substring(0, 1)
+                val key = digits.substring(0, 1)
                 for (digits in lastList) {
                     for (item in map[key]!!) {
                         val dataItem = digits.plus(item).trim()
                         result.add(dataItem)
                     }
                 }
-                return letterCombinations(digits.substring(1, digits.length),result)
+                return letterCombinations(digits.substring(1, digits.length), result)
             }
             return letterCombinations(digits, mutableListOf(""))
+        }
+
+        fun solveTwo(digits: String): List<String> {
+            val map = hashMapOf(
+                '2' to listOf('a', 'b', 'c'),
+                '3' to listOf('d', 'e', 'f'),
+                '4' to listOf('g', 'h', 'i'),
+                '5' to listOf('j', 'k', 'l'),
+                '6' to listOf('m', 'n', 'o'),
+                '7' to listOf('p', 'q', 'r', 's'),
+                '8' to listOf('t', 'u', 'v'),
+                '9' to listOf('w', 'x', 'y', 'z')
+            )
+            val result = mutableListOf<String>()
+
+            fun letterCombinations(index: Int, path:MutableList<Char>) {
+                // 长度相等，移除
+                if (index == digits.length) {
+                    result.add(path.joinToString(separator = ""))
+                    return
+                }
+                val list = map[digits[index]]
+                list?.let {
+                    it.forEach { item ->
+                        path.add(item)
+                        letterCombinations(index + 1, path)
+                        path.removeAt(path.size - 1)
+                    }
+                }
+            }
+            if (digits.isNullOrEmpty()) {
+                return result
+            }
+            letterCombinations(0, mutableListOf())
+            return result
         }
     }
 }
