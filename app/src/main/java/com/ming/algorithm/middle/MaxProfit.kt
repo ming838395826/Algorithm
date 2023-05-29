2936392ccb24965b8a6950ce37abe283450ee4c8
a <857813214@qq.com>
2023-05-30 00:31:56 +0800

    提交 122. 买卖股票的最佳时机 II

--- app/src/main/java/com/ming/algorithm/middle/MaxProfit.kt
+++ app/src/main/java/com/ming/algorithm/middle/MaxProfit.kt
@@ -46,5 +46,37 @@ class MaxProfit {
             }
             return result
         }
+
+        /**
+         * 动态规范
+         * 定义； F[n][0] 表示第n天持有股票所得现金
+         *       F[n][1] 表示第n天不持有股票所得最多现金
+         *       如果第i天持有股票即dp[i][0]， 那么可以由两个状态推出来
+         *          1.第i-1天就持有股票，那么就保持现状，所得现金就是昨天持有股票的所得现金 即：dp[i - 1][0]
+         *          2.第i天买入股票，所得现金就是昨天不持有股票的所得现金减去 今天的股票价格 即：dp[i - 1][1] - prices[i]
+         *          公式还是和只能买卖一次一样，只不过买入不是-prices[i]
+         * 初始化： dp[0][0] -prices[0] dp[0][1] = 0
+         * 顺序： 还是从小到大
+         */
+        fun solveTwo(prices: IntArray): Int {
+            if (prices.isEmpty()) return 0
+            val result = Array(prices.size) { IntArray(2) }
+            result[0][0] = -prices[0]
+            for (i in 1 until prices.size) {
+                result[i][0] = Math.max(
+                    //前面一直持有的
+                    result[i-1][0],
+                    // 当天买入
+                    result[i-1][1] - prices[i]
+                )
+                result[i][1] = Math.max(
+                    //前面一直持有的
+                    result[i-1][1],
+                    // 当天卖出
+                    result[i-1][0] + prices[i]
+                )
+            }
+            return result[prices.size - 1][1]
+        }
     }
 }
