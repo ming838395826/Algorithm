ba221ce59d3c19a89f61da4ce4ff65dc74dcde96
ming <838395826@qq.com>
2023-09-11 23:43:01 +0800

    提交 1003. 检查替换后的词是否有效

---
+++ app/src/main/java/com/ming/algorithm/middle/IsValid.kt
@@ -0,0 +1,54 @@
+package com.ming.algorithm.middle
+
+import com.ming.algorithm.bean.Node
+import com.ming.algorithm.bean.TreeNode
+import java.util.*
+
+/**
+ * @Description 检查替换后的词是否有效
+ * @Author ming
+ * @Date 2022/2/10 23:20
+ */
+class IsValid {
+
+    companion object {
+        /**
+        给你一个字符串 s ，请你判断它是否 有效 。
+        字符串 s 有效 需要满足：假设开始有一个空字符串 t = "" ，你可以执行 任意次 下述操作将 t 转换为 s ：
+        将字符串 "abc" 插入到 t 中的任意位置。形式上，t 变为 tleft + "abc" + tright，其中 t == tleft + tright 。注意，tleft 和 tright 可能为 空 。
+        如果字符串 s 有效，则返回 true；否则，返回 false。
+         */
+        @JvmStatic
+        fun main(args: Array<String>) {
+            /**
+            输入：s = "aabcbc"
+            输出：true
+            解释：
+            "" -> "abc" -> "aabcbc"
+            因此，"aabcbc" 有效。
+             */
+            print("结果是${solveOne("abccba")}\n")
+        }
+
+        /**
+         *  通过遍历堆占，把字符串放进去
+         *  大于3，判断到c 开始减去adb
+         *  最后为空则符合
+         */
+        fun solveOne(s: String): Boolean {
+            val stack = LinkedList<Char>()
+            s.forEach {
+                if ('c' == it) {
+                    "ba".forEach { item ->
+                        if (stack.size==0 || stack.pop() != item) {
+                            return false
+                        }
+                    }
+                } else {
+                    stack.push(it)
+                }
+            }
+            return stack.isEmpty()
+        }
+    }
+}
